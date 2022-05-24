6c97ca1879458ad7c773b18f1656f927363024a9
vovik541 <conanzhill@gmail.com>
2022-05-24 12:10:26 +0300

    util tests added

--- src/main/java/com/coursework/graph/util/Utility.java
+++ src/main/java/com/coursework/graph/util/Utility.java
@@ -1,12 +1,4 @@
 package com.coursework.graph.util;
-
-import com.coursework.graph.javafxappication.JavaFxApplication;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import lombok.SneakyThrows;
-
-import java.util.Objects;
-
 public class Utility {
     public static String transformNodeId(int id){
         return "Node_" + id;
--- src/test/java/com/coursework/graph/util/UtilityTest.java
+++ src/test/java/com/coursework/graph/util/UtilityTest.java
@@ -2,27 +2,42 @@ package com.coursework.graph.util;
 
 import org.junit.jupiter.api.Test;
 
+import static com.coursework.graph.util.Utility.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilityTest {
 
+    private static final int TEST_NODE_ID = 1;
+    private static final int TEST_BEGIN_NODE_ID = 1;
+    private static final int TEST_END_NODE_ID = 2;
+
     @Test
-    void transformNodeId() {
+    void transformNodeIdTest() {
+        String result = transformNodeId(TEST_NODE_ID);
+        assertEquals(result, "Node_1");
     }
 
     @Test
-    void getIdFromEdgeFullId() {
+    void getIdFromEdgeFullIdTest() {
+        int result = getIdFromEdgeFullId("Edge_1");
+        assertEquals(result, TEST_NODE_ID);
     }
 
     @Test
-    void transformEdgeId() {
+    void transformEdgeIdWithAdvancedParamsTest() {
+        String result = transformEdgeId(TEST_BEGIN_NODE_ID, String.valueOf(TEST_END_NODE_ID));
+        assertEquals(result, "Edge_1_2");
     }
 
     @Test
-    void testTransformEdgeId() {
+    void transformEdgeIdWithPrimitivesParamsTest() {
+        String result = transformEdgeId(TEST_BEGIN_NODE_ID, TEST_END_NODE_ID);
+        assertEquals(result, "Edge_1_2");
     }
 
     @Test
-    void transformTextId() {
+    void transformTextIdTest() {
+        String result = transformTextId(TEST_NODE_ID);
+        assertEquals(result, "Text_1");
     }
 }
